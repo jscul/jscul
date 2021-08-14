b09cb7f9a84577a42041eb0f676632c99c0dff70
jscul <cullen.scott.john@gmail.com>
2021-08-14 14:46:48 -0700

    Upgraded encryption strategy

--- javascript/encryptFile.js
+++ javascript/encryptFile.js
@@ -1,13 +1,19 @@
 const fs = require(`fs`);
 const crypto = require(`crypto`);
 
-const key = `14189dc35ae35e75ff31d7502e245cd9bc7803838fbfd5c773cdcd79b8a28bbd`;
+const KEY = Buffer.from(
+	`NVcSrW0L/qZ9TxllNAB4BAS53XqliRLRw6OQB6GjUeI=`,
+	"base64"
+);
+
+let key = crypto.createHash("sha256").update(String(`secret`));
 
 const encryptFile = () => {
-	const input = fs.createReadStream(`./pictures.tar.xz`);
-	const output = fs.createWriteStream(`./1.encrypted`);
+	const iv = crypto.randomBytes(16);
+	const input = fs.createReadStream(`./in.zip`);
+	const output = fs.createWriteStream(`./${iv.toString("hex")}.encrypted`);
 
-	const cipher = crypto.createCipher(`aes-256-cbc`, key);
+	const cipher = crypto.createCipheriv(`aes-256-cbc`, KEY, iv);
 	input.pipe(cipher).pipe(output);
 
 	output.on(`finish`, function () {
@@ -15,11 +21,17 @@ const encryptFile = () => {
 	});
 };
 
-const decryptFile = () => {
-	const input = fs.createReadStream(`./1.encrypted`);
-	const output = fs.createWriteStream(`./pictures2.tar.xz`);
+const decryptFile = (fileName) => {
+	const path = fileName.split("/");
+	const [iv, ext] = path[path.length - 1].split(".");
+	const input = fs.createReadStream(fileName);
+	const output = fs.createWriteStream(`./out.zip`);
 
-	const decipher = crypto.createDecipher(`aes-256-cbc`, key);
+	const decipher = crypto.createDecipheriv(
+		`aes-256-cbc`,
+		KEY,
+		Buffer.from(iv, "hex")
+	);
 	input.pipe(decipher).pipe(output);
 
 	output.on(`finish`, function () {
@@ -27,5 +39,5 @@ const decryptFile = () => {
 	});
 };
 
-encryptFile();
-// decryptFile();
+// encryptFile();
+decryptFile(`b6583778c28fcff7a3f962564ce6a516.encrypted`);
